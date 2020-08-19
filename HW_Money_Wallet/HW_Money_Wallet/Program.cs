648f95c7b964670818aeee96fc55e1e2fd4ebbc8
Kateřina Trčková <trckovakaterina@gmail.com>
2020-08-19 21:26:21 +0200

    Domácí úkol - pohyb peněz v peněžence

---
+++ HW_Money_Wallet/HW_Money_Wallet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW_Money_Wallet
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wallet1 = new Wallet();
+            wallet1.AddMoney(10000);
+            wallet1.AddMoney(3500);
+            wallet1.WithdrawMoney(2000);
+            wallet1.AddMoney(500);
+            wallet1.WithdrawMoney(3000);
+            wallet1.CurrentMoney();
+
+            Console.Read();
+        }
+    }
+}
---
+++ HW_Money_Wallet/HW_Money_Wallet/Wallet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HW_Money_Wallet
+{
+    public class Wallet
+    {
+        private int _currentMoney = 0;
+
+        public void AddMoney(int moneyToAdd)
+        {
+            if (moneyToAdd <= 0)
+            {
+                Console.WriteLine("Vkládaná částka musí být větší než 0.");
+            }
+            else if ((_currentMoney + moneyToAdd) > 5000)
+            {
+                Console.WriteLine("Nedošlo k vložení peněz do peněženky. Celková částka v peněžence nesmí být větší než 5 000 Kč.");
+            }
+            else
+            {
+                _currentMoney += moneyToAdd;
+            }
+        }
+
+        public void WithdrawMoney(int moneyToWithdraw)
+        {
+            if (moneyToWithdraw <= 0)
+            {
+                Console.WriteLine("Vámi požadovanou částku nelze vybrat. Ověřte správnost požadavku.");
+            }
+            else if (_currentMoney < moneyToWithdraw)
+            {
+                Console.WriteLine("Z peněženky nelze vybrat více peněz, než kolik se v ní aktuálně nachází.");
+            }
+            else
+            {
+                _currentMoney -= moneyToWithdraw;
+            }
+        }
+
+        public void CurrentMoney()
+        {
+            Console.WriteLine(_currentMoney);
+        }
+    }
+}
