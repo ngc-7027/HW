7b80bb521bd72fdc14dd4767e1d8a7298a9daf56
Kateřina Trčková <trckovakaterina@gmail.com>
2020-09-06 11:49:41 +0200

    Shape class

---
+++ HW_Shape/HW_Shape/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HW_Shape
+{
+    public abstract class Shape
+    {
+        protected void ControlParameters(params double[] parameters)
+        {
+            foreach (var parameter in parameters)
+            {
+                if (parameter <= 0 || parameter > double.MaxValue)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public abstract double GetArea(params double[] lengths);
+
+        public abstract double GetCircumference(params double[] lengths);
+    }
+}
