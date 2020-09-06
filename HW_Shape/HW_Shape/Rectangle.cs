8d635d3948d9450015d93d97e24a2ef97124c41d
Kateřina Trčková <trckovakaterina@gmail.com>
2020-09-06 11:49:53 +0200

    Rectangle class

---
+++ HW_Shape/HW_Shape/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿namespace HW_Shape
+{
+    public class Rectangle : Shape
+    {
+        public override double GetArea(params double[] lengths)
+        {
+            ControlParameters(lengths);
+
+            var l = lengths[0];
+            var w = lengths[1];
+
+            var result = l * w;
+            return result;
+        }
+
+        public override double GetCircumference(params double[] lengths)
+        {
+            ControlParameters(lengths);
+
+            var l = lengths[0];
+            var w = lengths[1];
+
+            var result = 2 * (l + w);
+            return result;
+        }
+    }
+}
