3af9b1a4bc0acaa59d52b5e3f7224a442b3bb194
Kateřina Trčková <trckovakaterina@gmail.com>
2020-09-06 11:50:15 +0200

    Triangle class

---
+++ HW_Shape/HW_Shape/Triangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HW_Shape
+{
+    public class Triangle : Shape
+    {
+        public override double GetArea(params double[] lengths)
+        {
+            ControlParameters(lengths);
+
+            var a = lengths[0];
+            var b = lengths[1];
+            var c = lengths[2];
+
+            var s = (a + b + c) / 2;
+            var result = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            return result;
+        }
+
+        public override double GetCircumference(params double[] lengths)
+        {
+            ControlParameters(lengths);
+
+            var a = lengths[0];
+            var b = lengths[1];
+            var c = lengths[2];
+
+            var result = a + b + c;
+            return result;
+        }
+    }
+}
