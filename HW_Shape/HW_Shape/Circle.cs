9aed974c567b98e9f3efd3ad60d2619b07edeada
Kateřina Trčková <trckovakaterina@gmail.com>
2020-09-06 11:50:04 +0200

    Circle class

---
+++ HW_Shape/HW_Shape/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HW_Shape
+{
+    public class Circle : Shape
+    {
+        public override double GetArea(params double[] lengths)
+        {
+            ControlParameters(lengths);
+
+            var r = lengths[0];
+
+            var result = Math.PI * Math.Pow(r, 2);
+            return result;
+        }
+
+        public override double GetCircumference(params double[] lengths)
+        {
+            ControlParameters(lengths);
+
+            var r = lengths[0];
+
+            var result = 2 * Math.PI * r;
+            return result;
+        }
+    }
+}
